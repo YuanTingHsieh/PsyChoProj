5abf6a43aa37587eb478122fa49ecdee97df9b19
YuanTingHsieh <b01901046@ntu.edu.tw>
2015-12-05 15:36:03 +0800

    Add index.html

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,26 +5,37 @@ var querystring = require("querystring"),
 function start(response) {
   console.log("Request handler 'start' was called.");
 
-  var body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html; '+
-    'charset=UTF-8" />'+
-    '</head>'+
-    '<body>'+
-    '<form action="/upload" enctype="multipart/form-data" '+
-    ' method="post">'+
-    '<input type ="file" name="upload">'+
+  fs.readFile(__dirname+'/'+'index.html', "utf8", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write(file);
+      response.end();
+    }
+  });
+//  var body = '<html>'+
+//    '<head>'+
+//    '<meta http-equiv="Content-Type" content="text/html; '+
+//    'charset=UTF-8" />'+
+//    '</head>'+
+//    '<body>'+
+//    '<form action="/upload" enctype="multipart/form-data" '+
+//    ' method="post">'+
+//    '<input type ="file" name="upload">'+
 //  '<form action="/upload" method="post">'+
 //  '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Upload file" />'+
-    '</form>'+
-    '</body>'+
-    '</html>';
+//    '<input type="submit" value="Upload file" />'+
+//    '</form>'+
+//    '</body>'+
+//    '</html>';
 
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(body);
-    response.end();
+//    response.writeHead(200, {"Content-Type": "text/html"});
+//    response.write(body);
+//    response.end();
 }
 
 function upload(response, request) {
