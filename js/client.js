613477d645cf60b7fe2436cf0c53f78a05069acd
YuanTingHsieh <b01901046@ntu.edu.tw>
2015-12-09 00:19:48 +0800

    Add something

---
+++ js/client.js
@@ -0,0 +1,45 @@
+(function(){
+	var Client = {
+		so=null;
+		isFirstConnect = true;
+		user = {"uname":null};
+	};
+	Client.connect = function (host, port){
+		//port or what??
+		var p = port||so, self = this;
+		if (this.so==null)
+		{
+			this.so = io.connect("http://"+host+":"+p, ('reconnect':false));
+			if (this.so)
+			{
+				this.so.on("connect",function(){self.doConnect();});
+				this.so.on("error",function(data){this.so=null;alert("Connection Failed!");});
+			}
+		}
+		else
+		{
+			this.so.socket.reconnect();
+		}
+	};
+	Client.login = function(callback)
+	{
+		this.so.emit("login",("uname":this.user.uname), function(data){callback(data);});
+	};
+	Client.doConnect = function()
+	{
+		if (this.isFirstConnect)
+		{
+			this.isFirstConnect = false;
+			this.bindEvent();
+		}
+		else
+		{
+			this.so.socket.reconnect();
+		}
+	};
+	Client.bindEvent = function()
+	{
+	};
+	window.Client = Client;
+	
+})();
