62faa6f35ce8b29ce10b91d0cf7a359bbae80b55
YuanTingHsieh <b01901046@ntu.edu.tw>
2015-12-13 18:06:26 +0800

    ffff

--- clientoserver.js
+++ clientoserver.js
@@ -3,13 +3,20 @@
         this.srv = server;
         this.so = socket;
         this.user = {"uname":null,"level":0,"score":0};
+        this.playertype = 0;
+        this.roomnum = 0;
+        this.playernum = 0 ;
+
+        this.money = 0;
+        this.decval =0;
         //綁定事件
         this.bindEvent();
     };
     Client.prototype = {
         toString:function()
         {
-           return "[uname:"+this.user.uname+",sid:"+this.so.id+"]";
+           return "[uname:" + this.user.uname + ",player:"+ this.playertype
+           + ",room:" + this.roomnum + ",sid:" +this.so.id+ "]";
         },
         bindEvent:function()
         {
@@ -20,6 +27,9 @@
                 this.so.on("disconnect",function(){self.doDisconnect();});
                 //註冊登陸事件
                 this.so.on("login",function(data,fn){self.doLogin(data,fn);});
+                //this.so.emit("sendmessage",{"mes":self.sendMessage()});
+                this.so.on("startgame",function(){self.doStartGame();});
+                this.so.on("money",function(data){self.doClitoSer(data);});
             }
         },
         //處理斷開連接
@@ -35,13 +45,17 @@
             {
                 this.user.uname = data.uname;
                 var isExists = this.srv.isUserExists(this);
-                console.log("client.js - Client "+this.user.uname+" has login as player "
-                    +this.srv.clicount+" in room "+this.srv.roomcount);
+                this.playertype = this.srv.clicount;
+                this.roomnum = this.srv.roomcount;
+                this.playernum = this.srv.clients.length;
+                //console.log("client.js - Client "+this.user.uname+" has login as player "
+                //    +this.srv.clicount+" in room "+this.srv.roomcount);
+                console.log("client.js - "+this.toString()+" has login");
                 //通知客戶端;
                 if(!isExists)
                 {
                     //callback to be 0
-                    fn(0,this.srv.clicount);
+                    fn(0,this.srv.clicount,this.srv.totalRound);
                     //this.srv.updateUserInfo();            
                 }
                 else 
@@ -50,6 +64,24 @@
                     this.srv.removeClientByID(this.so.id);
                 }
             }        
+        },
+        dosendMessage:function(message)
+        {
+            this.so.emit('sendMessage',{'mes':message});
+        },
+        doGameready:function(stat, roomn)
+        {
+            this.so.emit('gameready',{'room': roomn ,'ready': stat });
+        },
+        doStartGame:function()
+        {
+            this.srv.startGame(this);
+        },
+        doClitoSer:function(data)
+        {
+            console.log("client.js - Received "+data.val
+                +" from player "+this.playertype+" of room "+this.roomum);
+            this.money += data.val;
         }
     }
     exports.newClient = function(server,socket)
--- server.js
+++ server.js
@@ -6,8 +6,11 @@
     var Server = function(){
         this.io = null;
         this.clicount = 0;
-        this.roomcount = 1;
+        this.roomcount = 0;
         this.clients = [];
+        this.rooms = [];
+        this.totalRound = 4;
+        this.startMoney = 100;
     };
 
     Server.prototype = {
@@ -21,7 +24,6 @@
             }
             var srv = http.createServer(onRequest);
             //var srv = http.createServer(function(req,rep){});
-            //LLLLLLL
             //192.168.43.201
             //140.112.249.166
             srv.listen(port);
@@ -36,17 +38,37 @@
         bindEvent:function()
         {
             var self = this;
+
             this.io.sockets.on("connection",function(socket){
                
                 self.clicount++;
+                // 3 reset to 1, start new room
                 if (self.clicount===3)
                 {
                     self.clicount=1;
-                    self.roomcount++;
                 }
-                console.log("server.js - clinum is "+self.clicount);
-                console.log("server.js - Roomnum is "+self.roomcount);
+                
+                
                 self.doConnect(socket);
+
+                //see if game ready
+                if (self.clicount===2)
+                {
+                    self.rooms[self.rooms.length-1].valid = true;
+                    self.sendStatus(self.clients[self.clients.length-1],true,self.rooms.length);
+                    self.sendStatus(self.clients[self.clients.length-2],true,self.rooms.length);
+                }
+                else
+                {
+                    self.roomcount++;
+                    self.rooms.push({"num":self.roomcount,"money":self.startMoney,
+                        "valid":false, "rounds":self.totalRound, "activate":false});
+                    //self.roomValids.push(false);
+                    self.sendStatus(self.clients[self.clients.length-1],false,self.rooms.length);
+                }
+
+                console.log("server.js - clinum is "+self.clicount);
+                console.log("server.js - Total Room is "+self.rooms.length);
                 //These lines below are tesing socket.html
                 //testing
                 //setInterval(function() {
@@ -81,10 +103,33 @@
                 }
             }
             if(idx!=-1){
-                 this.clients.splice(idx,1);
-                 console.log("server.js - removing client:"+sID);
+                console.log("server.js - removing client:"+sID);
+                var mes = "Your opponent has left the game, please reconnect.";
+                if(this.rooms[this.clients[idx].roomnum-1].valid===true)
+                {
+                    if(this.clients[idx].player===1)
+                    {
+                        this.sendMessage(this.clients[idx+1],mes);
+                    }
+                    else
+                    {
+                        this.sendMessage(this.clients[idx-1],mes);
+                    }
+                    this.rooms[this.clients[idx].roomnum -1].valid = false;
+                }
+                this.clients.splice(idx,1);
+                //ggg 
             }
             //如果所有客戶端都退出，則遊戲復位
+            if (this.clients.length===0)
+            {
+                //this.resetServer();
+                console.log("server.js - Server is resetting...");
+                this.clicount = 0;
+                this.roomcount = 0;
+                
+                this.rooms = [];
+            }
             
         },
             //判斷用戶是否存在
@@ -98,6 +143,58 @@
                 }
             }
             return false;
+        },
+        sendMessage:function(client, message)
+        {
+            console.log("server.js - sending message to "+client.toString());
+            client.dosendMessage(message);
+        },
+        sendStatus:function(client, stat, roomnum)
+        {
+            client.doGameready(stat, roomnum);
+        },
+        startGame:function(client)
+        {
+            //var self =this;
+            if((client.playertype===1)&&(this.rooms[client.roomnum-1].activate===false))
+            {
+                var nowroom = client.roomnum,
+                    nowplayer = client.playernum;
+                var data1,data2;
+                data1 = this.dealFirst(nowroom, nowplayer);
+
+                for(var nowround = 1;nowround<totalRound;nowround++)
+                {
+                    if(nowround%2===0)
+                    {
+                        data1 = this.dealPlayerOne(nowroom, nowplayer);
+                    }
+                    else
+                    {
+                        data2 = this.dealPlayerTwo(nowroom, nowplayer+1);
+                    }
+                }
+                this.rooms[client.roomnum-1].activate=true;
+            }
+            else
+            {
+                console.log("Room has activated.");
+            }
+        },
+        dealFirst:function(nowroom,playernum)
+        {
+            var dec = this.clients[playernum-1].doClitoSer()
+            this.rooms[nowroom-1].money += ;
+        }
+        dealPlayerOne:function(nowroom, playernum)
+        {
+            this.sendMoney()
+        }
+        sendMoney:function(playernum, money)
+        {},
+        roundComplete:function()
+        {
+            //need to save things
         }
     }
 
