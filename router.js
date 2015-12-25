bcb41db1ca9e6cd98e2367f22a8dd9563a118b68
YuanTingHsieh <b01901046@ntu.edu.tw>
2015-12-25 16:57:44 +0800

    removing irrelevant file

--- router.js
+++ router.js
@@ -1,9 +1,7 @@
 function route(handle, pathname, response, request) {
-  //console.log("router.js - About to route a request for "+pathname);
   if (typeof handle[pathname] === 'function' ) {
     handle[pathname](response,request);
   } else {
-    //console.log("router.js - No request handler found for "+pathname);
     response.writeHead(404, {"Content-Type": "text/plain"});
     response.write("404 Not found");
     response.end();
